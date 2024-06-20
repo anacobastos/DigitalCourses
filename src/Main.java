f62c07a285a2ac5abe5c52b39019548d8fe249d6
anacobastos <anitabastos16@hotmail.com>
2024-06-20 12:19:52 -0300

    "Animal with lombok"

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
+import Builder.Animal;
+import Builder.Pessoa;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Pessoa novaPessoa = new Pessoa.PessoaBuilder()
+                .apelido("Carol")
+                .nome("Ana Caroline")
+                .sobrenome("Bastos")
+                .email("anacaroline.bastos@outlook.com")
+                .dataDeNascimento(LocalDate.of(1997,07,06))
+                .build();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(novaPessoa);
+
+        Animal boo = Animal.builder()
+                .nome("Boobidi")
+                .raça("Vira Lata")
+                .personalidade("O melhor cachorro do mundo")
+                .idade("2 anos e meio")
+                .build();
+
+        System.out.println(boo);
     }
 }
